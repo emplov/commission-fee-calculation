8cf5b11cc35c77285d966dc3a26048130526cf4e
shamsuddinkhojaev-au <shamsuddinkhojaev_au@groupbwt.com>
2022-02-09 00:17:58 +0500

    fix: removed test case

--- tests/ParsersTest.php
+++ tests/ParsersTest.php
@@ -29,28 +29,4 @@ class ParsersTest extends BaseTest
             '8607.39',
         ], $data['response'],  'Returned data not same.');
     }
-
-    public function testCheckTxt()
-    {
-        $parser = new Dispatcher('input.txt', ',', '"', '\\');
-
-        $data = $parser->parse();
-
-        $this->assertSame(13, count($data['response']), 'Data return count not same.');
-        $this->assertSame([
-            '0.60',
-            '3.00',
-            '0.00',
-            '0.06',
-            '1.50',
-            '0.00',
-            '0.69',
-            '0.30',
-            '0.30',
-            '3.00',
-            '0.00',
-            '0.00',
-            '8607.39',
-        ], $data['response'],  'Returned data not same.');
-    }
 }
