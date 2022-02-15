3b365d0ecd55280fac58d7185a90bc439926d74e
shamsuddinkhojaev-au <shamsuddinkhojaev_au@groupbwt.com>
2022-02-15 19:47:35 +0500

    feat: removed dto abstract

---
+++ src/DTO/CommissionDataDTO.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommissionFeeCalculation\DTO;
+
+class CommissionDataDTO
+{
+    public function __construct(
+        public int $userKey,
+        public string $date,
+        public string $operationAmount,
+        public string $operationCurrency,
+        public string $userType,
+        public int $decimalsCount,
+    ) {
+    }
+}
